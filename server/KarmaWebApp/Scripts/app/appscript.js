09d3ec548660d718bec51316ba3f207dfc5cd140
makarandp0 <makarandp@yahoo.com>
2013-12-25 19:16:03 -0800

    using localstorage, fixed hehimhis, added wizard for request

--- server/KarmaWebApp/Scripts/app/appscript.js
+++ server/KarmaWebApp/Scripts/app/appscript.js
@@ -1,8 +1,25 @@
 ﻿
 var ViewModelClass = (function (name, pic) {
     function ViewModel(name, pic) {
+        var self = this;
         this.personName = name;
         this.pictureUrl = pic;
+        this.firstName = function (person) {
+            return person.name.split(" ")[0].substring(0, 20); // get 1st part before space, and return only 1st 20 characters.
+        }
+        this.hehimhis = function (person, hehimhis) {
+            var returnvalue = hehimhis;
+            if (person.gender === "female") {
+                if (hehimhis === "he") return "she";
+                if (hehimhis === "He") return "She";
+                if (hehimhis === "his") return "her";
+                if (hehimhis === "His") return "Her";
+                if (hehimhis === "him") return "her";
+                if (hehimhis === "Him") return "Her";
+                console.log("Error: what do you mean by:" + hehimhis);
+            }
+            return returnvalue;
+        };
 
         // my friends.
         this.selectedfriend = ko.observable();
@@ -15,6 +32,9 @@ var ViewModelClass = (function (name, pic) {
 
         // new request
         this.requestText = ko.observable("");         // request header text value.
+        this.requestDateTime = ko.observable("");     // request date/time.
+        this.request_location = ko.observable("");    // request location.
+        this.requestOnPage = ko.observable(1);        // which page are we on.
         this.showRequestError = ko.observable(false); // need to show error message about next button?
 
         // inbox
@@ -35,24 +55,72 @@ var ViewModelClass = (function (name, pic) {
             this.selectedoutbox(item);
         };
 
-        this.CreateRequest = function () {
+        // invite friends
+        this.inviteText = ko.observable("Hello friends, Please join karmaweb!");
+        this.InviteFriends = function () {
+            alert("implement this:" + this.inviteText());
+        };
+
+        this.GotoPrevPage = function () {
+            self.requestOnPage(self.requestOnPage()-1 );
+        }
+
+        this.GotoNextPage = function () {
             if (this.requestText().length < 5) {
 
                 // if request is not value
                 // show error message
                 // and setup callback to remove the error message.
-                this.showRequestError(true);
+                self.showRequestError(true);
                 $('#textticker').focus(function () {
-                    myViewModel.showRequestError(false);
+                    self.showRequestError(false);
                 });
 
             } else {
+                self.requestOnPage(self.requestOnPage() + 1);
+            }
+        };
+
+        this.CreateRequest = function () {
+            {
                 $.post('/Api/createrequest', { 'Title': this.requestText() },
                         function (data, statusText) {
                             console.log('server returned. data.error ' + data.error + ', data.errorcode:' + data.errorcode + ', statusText' + statusText);
                         });
             }
         };
+        function showposition(position) {
+            console.log("got current location");
+            console.log("lat:" + position.coords.latitude);
+            console.log("lan:" + position.coords.longitude);
+            self.request_location("lat:" + position.coords.latitude + ",lan:" + position.coords.longitude);
+        }
+        function showError(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    console.log("User denied the request for Geolocation.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    console.log("Location information is unavailable.");
+                    break;
+                case error.TIMEOUT:
+                    console.log("The request to get user location timed out.");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    console.log("An unknown error occurred.");
+                    break;
+            }
+        }
+        this.UseCurrentLocation = function () {
+            console.log("WIll user current location");
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showposition, showError);
+            }
+            else {
+                console.log("!navigator.geolocation");
+            }
+        }
+
         this.BlockSelectedFriend = function () {
             this.selectedfriend().blocked = true;
             this.selectedfriend.valueHasMutated();
