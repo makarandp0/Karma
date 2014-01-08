fc22eab6f36db0987ee635f84f4932c033ed5822
makarandp0 <makarandp@yahoo.com>
2014-01-08 00:16:10 -0800

    fixed viewport for IE10

--- server/KarmaWebApp/Scripts/app/apptypes.ts
+++ server/KarmaWebApp/Scripts/app/apptypes.ts
@@ -491,3 +491,14 @@ module KarmaTypes {
         }
     }
 } 
+
+// to fix ms viewport :(
+(function () {
+    if ("-ms-user-select" in document.documentElement.style && navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement("style");
+        msViewportStyle.appendChild(
+            document.createTextNode("@-ms-viewport{width:auto!important}")
+            );
+        document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+    }
+})();
